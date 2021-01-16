7514d9f164fb53353b9e50020f496aec3dcf24ac
Tofunmi Osho <oshotofunmi@gmail.com>
2021-01-16 02:13:11 +0100

    add export paths

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   images: {
     domains: ['firebasestorage.googleapis.com']
-  }
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/shop': { page: '/shop', query: { title: 'Home' } },
+      '/login': { page: '/login', query: { title: 'Log in' } },
+      '/signup': { page: '/signup', query: { title: 'Sign up' } },
+      '/logout': { page: '/logout', query: { title: 'Log out' } },
+      '/cart': { page: '/cart', query: { title: 'Cart' } },
+      '/checkout': { page: '/checkout', query: { title: 'Checkout' } }
+    }
+  },
 }
